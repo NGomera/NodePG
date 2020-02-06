ca57548dc94034e3d556d56ad4ae8ba9cbcf1446
NGomera <gomera.it111@gmail.com>
2020-02-06 12:22:15 +0800

    Wala pa Nahuman :(

---
+++ gomera-prob1.js
@@ -0,0 +1,17 @@
+const { Pool } = require('pg')
+
+ const pool = new Pool({
+ user: `${process.env.DB_USER}`,
+ host: `${process.env.DB_HOST}`,
+ database: `${process.env.DB_DATABASE}`,
+ password: `${process.env.DB_PASSWORD}`,
+ port: `{process.env.DB_PORT}`,
+ ssl: true,
+ })
+
+ pool.query('SELECT pet_name, name, occupation from "Gomera_pet".pets','INNER JOIN "Gomera_pet".owners ON pets.owner_id = owners.owner_id WHERE occupation' = 'Programmer', (error, results) => {
+ if (error) {
+ throw error
+ }
+ console.log(results.rows)
+ })
